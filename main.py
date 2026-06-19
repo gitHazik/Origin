177344cdd66c79234f6ff969e44ec1fd28f67784
gitHazik <hazikkhalid700@gmail.com>
2026-06-19 20:57:19 +0530

    wind prop

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from direct.showbase.ShowBase import ShowBase
 from panda3d.core import loadPrcFileData
+from panda3d.core import WindowProperties
+
 
 
 loadPrcFileData(" ", """ 
-window-title My Game
+window-title Game
 win-size 1280 720
 fullscreen false    
 """)
@@ -14,7 +16,12 @@ class MyApp(ShowBase):
     def __init__(self):
         super().__init__()
 
+        #windows properties
+        props = WindowProperties()
+        props.setFixedSize(False)
+        self.win.requestProperties(props)
 
+        
 
 
 
